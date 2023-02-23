716b2e47b61d580d911eb594e623ce139a58bb77
SFarida <faridagueleu@gmail.com>
2023-02-23 12:30:55 +0100

    Fix linters

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,33 @@
-import { useState, useRef } from "react";
-import { useOnClickOutside } from "../useOnClickOutside";
+import { useState, useRef } from 'react';
+import { useOnClickOutside } from '../useOnClickOutside';
 
 const Modal = () => {
-    const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
-    const ref = useRef();
+  const openModalHandler = () => {
+    setOpenModal(true);
+  };
 
-    useOnClickOutside(ref, openModal, () => setOpenModal(false));
-    return (
-        <div className="modal">
-            <button onClick={() => setOpenModal(true)}>Modal</button>
-            {
+  const closeModalHandler = () => {
+    setOpenModal(false);
+  };
+
+  const ref = useRef();
+
+  useOnClickOutside(ref, openModal, () => setOpenModal(false));
+  return (
+    <div className="modal">
+      <button type="button" onClick={openModalHandler} onKeyDown={openModalHandler}>Modal</button>
+      {
                 openModal && (
-                    <div ref={ref} className="modalContent">
-                        <span onClick={() => setOpenModal(false)}>X</span>
-                        <div>Modal content here</div>
-                    </div>
+                <div ref={ref} className="modalContent">
+                  <span role="button" tabIndex={0} onClick={closeModalHandler} onKeyDown={closeModalHandler}>X</span>
+                  <div>Modal content here</div>
+                </div>
                 )
             }
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Modal;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,36 @@
-import { useState, useEffect, useRef } from "react";
-import { useOnClickOutside } from "../useOnClickOutside";
+import { useState, useRef } from 'react';
+import { useOnClickOutside } from '../useOnClickOutside';
 
 const Navbar = () => {
-    const [dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-    const ref = useRef();
+  const ref = useRef();
 
-    useOnClickOutside(ref, dropdown, () => setDropdown(false));
+  useOnClickOutside(ref, dropdown, () => setDropdown(false));
 
-    return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li ref={ref}>
-                    <button onClick={() => setDropdown((prev) => !prev)}>
-                        Services <span>&#8595;</span>
-                    </button>
-                    {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button type="button" onClick={() => setDropdown((prev) => !prev)}>
+            Services
+            {' '}
+            <span>&#8595;</span>
+          </button>
+          {
                         dropdown && (
-                            <ul>
-                                <li>Design</li>
-                                <li>Development</li>
-                            </ul>
+                        <ul>
+                          <li>Design</li>
+                          <li>Development</li>
+                        </ul>
                         )
                     }
-                </li>
-            </ul>
-        </nav>
-    )
-}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
