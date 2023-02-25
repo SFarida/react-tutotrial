51142675fabbde66e1aae900b0f85a8ea60684af
SFarida <faridagueleu@gmail.com>
2023-02-25 09:49:29 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import './styles/app.css';
 import Header from './components/Header';
 import TodosLogic from './components/TodosLogic';
 import Modal from './components/Modal';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Modal />
-      <TodosLogic />
-    </>
+    <div className="wrapper">
+      <div className="todos">
+        <Header />
+        <Modal />
+        <TodosLogic />
+      </div>
+    </div>
   );
 }
 
